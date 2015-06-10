50f68408fd903fcbbacbac018dee80cfd637e6ba
Eduardo <edubaik@gmail.com>
2015-06-10 08:01:17 -0700

    fixed comment attribute bug

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.create(comment: params[:comment], message: Message.find(params[:message_id]), user: User.find(params[:user_id]))
+    @comment = Comment.create(commnet: params[:comment], message: Message.find(params[:message_id]), user: User.find(params[:user_id]))
     redirect_to "/users/#{params[:user_id]}"
   end
 
